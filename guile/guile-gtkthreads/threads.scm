95c07bef5c6d1f02be6000ca4e0569336c1bb35d
mdj <>
2000-03-30 16:56:24 +0000

    * threads.scm (gtkthreads-handler?, gtkthreads-ensure-handler): New procedures.

--- guile/guile-gtkthreads/threads.scm
+++ guile/guile-gtkthreads/threads.scm
@@ -1,3 +1,22 @@
 (define-module (gtk threads)
-  :use-module (gtk guilegtkthreads))
+  :use-module (gtk guilegtkthreads)
+  :use-module (gtk gtk)
+  :use-module (ice-9 threads))
 
+(export gtkthreads-handler? gtkthreads-ensure-handler)
+
+(define handler-running? #f)
+
+(define (gtkthreads-handler?)
+  handler-running?)
+
+(define (gtkthreads-ensure-handler)
+  (if (not handler-running?)
+      (dynamic-wind
+	  (lambda ()
+	    (set! handler-running? #t))
+	  (lambda ()
+	    (begin-thread
+	     (gtk-main)))
+	  (lambda ()
+	    (set! handler-running? #f)))))
