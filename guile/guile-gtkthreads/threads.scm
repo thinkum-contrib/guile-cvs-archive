ba458a2070e68bcd04e66d249adf01c6e4bc763b
mdj <>
2000-04-08 23:27:56 +0000

    * threads.scm (gtkthreads-ensure-handler): Use gdk-threads-enter/leave.

--- guile/guile-gtkthreads/threads.scm
+++ guile/guile-gtkthreads/threads.scm
@@ -1,6 +1,7 @@
 (define-module (gtk threads)
   :use-module (gtk guilegtkthreads)
   :use-module (gtk gtk)
+  :use-module (gtk gdk)
   :use-module (ice-9 threads))
 
 (export gtkthreads-handler? gtkthreads-ensure-handler)
@@ -12,11 +13,13 @@
 
 (define (gtkthreads-ensure-handler)
   (if (not handler-running?)
-      (dynamic-wind
-	  (lambda ()
-	    (set! handler-running? #t))
-	  (lambda ()
-	    (begin-thread
-	     (gtk-main)))
-	  (lambda ()
-	    (set! handler-running? #f)))))
+      (begin-thread
+       (dynamic-wind
+	   (lambda ()
+	     (gdk-threads-enter)
+	     (set! handler-running? #t))
+	   (lambda ()
+	     (gtk-main))
+	   (lambda ()
+	     (set! handler-running? #f)
+	     (gdk-threads-leave))))))
