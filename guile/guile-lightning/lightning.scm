81a1e93a5b0d0f89ced4d8d25a4e21c8da303a7f
mvo <>
2001-03-25 04:31:36 +0000

    * lightning.scm (asm-macros, register-asm-macro, get-asm-macro, asm-macroexpand, define-asm-macro): New, export (register-asm-macro and define-asm-macro): (assemble): Pass instructions thru asm-macroexpand before assembling.

--- guile/guile-lightning/lightning.scm
+++ guile/guile-lightning/lightning.scm
@@ -1,5 +1,37 @@
 (define-module (lightning))
 
+(export assemble disassemble register-asm-macro define-asm-macro)
+
 (dynamic-call "scm_init_lightning" (dynamic-link "libguile-lightning"))
 
-(export assemble disassemble)
+(define asm-macros (make-hash-table 31))
+
+(define (register-asm-macro name transformer)
+  (hashq-set! asm-macros name transformer))
+
+(define (get-asm-macro name)
+  (hashq-ref asm-macros name #f))
+
+(define (asm-macroexpand insns)
+  (let loop ((res '())
+	     (insns insns))
+    (cond 
+     ((null? insns)
+      (reverse! res))
+     ((and (list? (car insns))
+	   (get-asm-macro (caar insns)))
+      => (lambda (transformer)
+	   (loop res
+		 (append! (apply transformer (cdar insns))
+			  (cdr insns)))))
+     (else
+      (loop (cons (car insns) res)
+	    (cdr insns))))))
+
+(define prim-assemble assemble)
+
+(define (assemble insns)
+  (prim-assemble (asm-macroexpand insns)))
+
+(define-macro (define-asm-macro head . body)
+  `(register-asm-macro ',(car head) (lambda ,(cdr head) ,@body)))
