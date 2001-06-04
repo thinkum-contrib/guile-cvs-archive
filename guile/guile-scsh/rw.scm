ae6e5ae2f6d760a701600569f7e1595ec483158d
ghouston <>
2001-06-04 22:06:13 +0000

    * rw.scm (generic-write-string/partial, write-string/partial): 	removed.  write-string/partial is now in (ice-9 rw).

--- guile/guile-scsh/rw.scm
+++ guile/guile-scsh/rw.scm
@@ -9,10 +9,10 @@
   :use-module (scsh let-opt)
 )
 (export bogus-substring-spec? read-string/partial
-	read-string! read-string write-string write-string/partial)
+	read-string! read-string write-string)
 
 ;; from (ice-9 rw)
-(export read-string!/partial)
+(export read-string!/partial write-string/partial)
 
 (define (bogus-substring-spec? s start end)
   (or (< start 0)
@@ -74,30 +74,30 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Non-blocking output to a buffered port is not defined.
 
-(define (generic-write-string/partial s start end writer target)
-  (if (bogus-substring-spec? s start end)
-      (error "Bad substring indices" writer s start end target))
+; (define (generic-write-string/partial s start end writer target)
+;   (if (bogus-substring-spec? s start end)
+;       (error "Bad substring indices" writer s start end target))
 
-  (if (= start end) 0			; Vacuous request.
-      (let loop ()
-	(catch 'system-error
-	       (lambda ()
-		 (let ((nwritten (writer s target start end start)))
-		   nwritten))
-	       (lambda args 
-		 (let ((err (car (list-ref args 4))))
-		   (cond ;; ((= err errno/intr) (loop)) ; handled by primitive.
-			((or (= err errno/wouldblock); No forward-progess here.
-			     (= err errno/again))
-			 0)
-			(else (apply scm-error args)))))))))
+;   (if (= start end) 0			; Vacuous request.
+;       (let loop ()
+; 	(catch 'system-error
+; 	       (lambda ()
+; 		 (let ((nwritten (writer s target start end start)))
+; 		   nwritten))
+; 	       (lambda args 
+; 		 (let ((err (car (list-ref args 4))))
+; 		   (cond ;; ((= err errno/intr) (loop)) ; handled by primitive.
+; 			((or (= err errno/wouldblock); No forward-progess here.
+; 			     (= err errno/again))
+; 			 0)
+; 			(else (apply scm-error args)))))))))
 
-(define (write-string/partial s . args)
-  (let-optionals args ((fd/port (current-output-port))
-		       (start 0)
-		       (end (string-length s)))
-		 (generic-write-string/partial s start end
-					 uniform-array-write fd/port)))
+; (define (write-string/partial s . args)
+;   (let-optionals args ((fd/port (current-output-port))
+; 		       (start 0)
+; 		       (end (string-length s)))
+; 		 (generic-write-string/partial s start end
+; 					 uniform-array-write fd/port)))
 
 
 ;;; Persistent writing
