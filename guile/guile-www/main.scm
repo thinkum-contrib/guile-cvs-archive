bc9a75264b97588c789222c9ee60c04121a831f8
ttn <>
2002-04-27 02:01:26 +0000

    Comment munging; nfc.

--- guile/guile-www/main.scm
+++ guile/guile-www/main.scm
@@ -1,22 +1,21 @@
-;;;; www/main.scm: General WWW navigation aids.
+;;; www/main.scm --- General WWW navigation aids.
 
-;;;; 	Copyright (C) 1997,2001 Free Software Foundation, Inc.
-;;;;
-;;;; This program is free software; you can redistribute it and/or modify
-;;;; it under the terms of the GNU General Public License as published by
-;;;; the Free Software Foundation; either version 2, or (at your option)
-;;;; any later version.
-;;;;
-;;;; This program is distributed in the hope that it will be useful,
-;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;;;; GNU General Public License for more details.
-;;;;
-;;;; You should have received a copy of the GNU General Public License
-;;;; along with this software; see the file COPYING.  If not, write to
-;;;; the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
-;;;; Boston, MA 02111-1307 USA
-;;;;
+;; 	Copyright (C) 1997,2001,2002 Free Software Foundation, Inc.
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this software; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+;; Boston, MA 02111-1307 USA
 
 ;;; Commentary:
 
@@ -34,9 +33,9 @@
 (define dispatch-table
   (acons 'http http:get '()))
 
-;;; (www:get URL)
-;;;   parse a URL into portions, open a connection, and retrieve
-;;;   selected document
+;; (www:get URL)
+;;   parse a URL into portions, open a connection, and retrieve
+;;   selected document
 
 (define-public (www:set-protocol-handler! proto handler)
   (set! dispatch-table
@@ -47,7 +46,7 @@
     ;; get handler for this protocol
     (case (url:scheme url)
       ((http) (let ((msg (http:get url)))
-		  (http:message-body msg)))
+                (http:message-body msg)))
       (else
        (let ((handle (assq-ref dispatch-table (url:scheme url))))
 	 (if handle
