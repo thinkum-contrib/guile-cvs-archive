75b80405b4bf917c9bb18277acd4afcffc560fc9
hanwen <>
2002-08-16 22:01:10 +0000

    * mallocs.c (scm_malloc_obj): use scm_gc_malloc in stead of malloc.
    * gc-segment.c (scm_i_get_new_heap_segment): remove cluster cruft:
    only use SCM_MIN_HEAP_SEG_SIZE.
    
    * ports.c (scm_add_to_port_table): add backwards compatibility
    function
    
    * ports.h: use scm_i_ prefix for port table and port table size.

--- guile/guile-core/libguile/coop.c
+++ guile/guile-core/libguile/coop.c
@@ -620,7 +620,7 @@ coop_create (coop_userf_t *f, void *pu)
   else
 #endif
     {
-      t = malloc (sizeof (coop_t));
+      t = scm_malloc (sizeof (coop_t), "coop");
 
       t->specific = NULL;
       t->n_keys = 0;
@@ -647,7 +647,7 @@ coop_create (coop_userf_t *f, void *pu)
       while (coop_child || mother_awake_p)
 	usleep (0);
 #else
-      t->sto = malloc (COOP_STKSIZE);
+      t->sto = scm_malloc (COOP_STKSIZE);
       sto = COOP_STKALIGN (t->sto, QT_STKALIGN);
       t->sp = QT_SP (sto, COOP_STKSIZE - QT_STKALIGN);
 #endif
@@ -730,7 +730,7 @@ coop_join(coop_t *t)
   /* Create a join list if necessary */
   if (t->joining == NULL)
     {
-      t->joining = malloc(sizeof(coop_q_t));
+      t->joining = scm_malloc(sizeof(coop_q_t));
       coop_qinit((coop_q_t *) t->joining);
     }
 
