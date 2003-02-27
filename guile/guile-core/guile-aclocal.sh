d0f44fa34a3315d32780594cf514388dee9bc6b2
rlb <>
2003-02-27 07:10:51 +0000

    * guile-aclocal.sh: deleted in favor of ACLOCAL_AMFLAGS in Makefile.am.

--- guile/guile-core/guile-aclocal.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# This trivial script exists to have a single place where the
-# invocation of aclocal is specified.  It is called from both
-# autogen.sh and the Makefile.
-
-aclocal -I guile-config
