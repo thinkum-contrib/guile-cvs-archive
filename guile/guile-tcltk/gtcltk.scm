8c55b1402a0c593a1f3a3126dfbfabb44283db1f
spchamp@users.noreply.github.com <>
2005-01-18 15:00:54 +0000

    This commit was manufactured by cvs2svn to create branch 'branch_mvo-thread- cleanup'.

--- guile/guile-tcltk/gtcltk.scm
+++
@@ -1,7 +0,0 @@
-(define-module (tcltk gtcltk)
-  :use-module (tcltk dynlink))
-
-(if (defined? 'load-extension)
-    (load-extension "libguile-tcltk-gtcltk" "scm_init_tcltk_gtcltk")
-    (merge-compiled-code "scm_init_gtcltk" "libguile-tcltk-gtcltk"))
-
--- guile/qscheme/v0.6/asm.h
+++
@@ -1,9 +0,0 @@
-#define SCM_ASM_INSTR 	struct _SCM_ASM_INSTR
-#define SCM_ASM_CODE 	struct _SCM_ASM_CODE
-
-SCM_ASM_INSTR {
-  char *name;
-  SOBJ 	*atom;
-  void  (*func)(SOBJ code, SOBJ argl);
-};
-
