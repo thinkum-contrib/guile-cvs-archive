0c28ae45c1963d854ee8d59f1a58459e795de7dd
mvo <>
2005-05-23 19:57:22 +0000

    The FSF has a new address.

--- guile/guile-core/ice-9/debugger/trap-hooks.scm
+++ guile/guile-core/ice-9/debugger/trap-hooks.scm
@@ -14,7 +14,7 @@
 ;; 
 ;; You should have received a copy of the GNU Lesser General Public
 ;; License along with this library; if not, write to the Free Software
-;; Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 ;;; This module provides an abstraction around Guile's low level trap
 ;;; handler interface; its aim is to make the low level trap mechanism
